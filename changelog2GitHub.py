12343301fe194fd6cae345703c17f58132dafd9f
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-02-01 08:28:34 +0000

    Updated md format

--- changelog2GitHub.py
+++ changelog2GitHub.py
@@ -1,26 +1,41 @@
+#!/usr/bin/python
+"""changelog2GitHub.py
+
+This simple tool converts changelog from maven-changes-plugin
+to Markdown format which is then displayed.
+
+Only last release tag (first in xml file) is processed,
+"""
+
 import xml.etree.ElementTree as ET
+import sys
 
 class bcolors:
-    HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-typeconvert = {'update': 'U', 'fix': 'B', 'add':'F'}
-changelog = '/home/baniuk/Documents/Repos/QuimP-env/QuimP/src/changes/changes.xml'
+typeconvert = {	'update': 'U',
+				'fix': 'B',
+				'add':'F'}
+
+if len(sys.argv)!=2:
+	print "Wrong number of arguments"
+	print "changelog2GitHub.py /path/to/changes.xml"
+	exit(1)
+
+# get file name
+changelog = sys.argv[1]
 
 e = ET.parse(changelog).getroot()
 
 # get latest release child assuming that it is first on list
-
 releases = list(list(e)[1])
-latest_issues = list(releases[0])
+latest_issues = list(releases[0]) # list of issues
 
-print bcolors.OKBLUE + "Doing " + str(latest_issues.__len__()) + " entries for tag " + releases[0].get("version") + bcolors.ENDC
+print bcolors.OKBLUE + bcolors.BOLD + "Doing " + str(latest_issues.__len__()) + " entries for tag " + releases[0].get("version") + bcolors.ENDC
 print 
 print bcolors.FAIL+"---->"+bcolors.ENDC
 
